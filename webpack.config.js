6d4420c5a964ca81793d875f60eed44589d72b97
sangey <sangeylama1000@gmail.com>
2021-06-25 10:32:37 +0545

    pulicpath

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: 'https://tenzin1000.github.io/css-grid-vue',
     filename: 'build.js'
   },
   module: {
