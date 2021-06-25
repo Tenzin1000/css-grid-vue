acd5fba8041114205fdc107daa4c012c3f9299ac
sangey <sangeylama1000@gmail.com>
2021-06-25 08:50:57 +0545

    changed

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, '/dist'),
-    publicPath: '/dist/',
+    publicPath: 'https://github.com/Tenzin1000/css-grid-vue/',
     filename: 'build.js'
   },
   module: {
